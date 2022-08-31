89a1b379024ddfb0c6a9667888baab5cdb245839
Logan <yuelong.chen.btr@gmail.com>
2022-08-31 19:46:47 +0800

    现在下好音乐吧

--- download_music.bash
+++ download_music.bash
@@ -9,3 +9,4 @@ music_list=https://www.youtube.com/playlist?list=PLOPongunjVMV9KhWBq1ByXVyGIXh41
 wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O ./youtube-dl
 chmod a+rx ./youtube-dl
 ./youtube-dl -x --yes-playlist --audio-format mp3  -o 'mp3s/%(title)s.%(ext)s' $music_list
+rm youtube-dl
