d74a5542c5b05a62dd081be1adca69d4b2244267
Yuelong Chen (Logan) <yuelong.chen.btr@gmail.com>
2022-08-31 18:43:03 +0800

    Update download_music.bash

--- download_music.bash
+++ download_music.bash
@@ -6,6 +6,6 @@ set -e
 
 music_list=https://www.youtube.com/playlist?list=PL6rKmks47C2vRvYpBy51BQV2oHwFpljql
 
-wget https://yt-dl.org/downloads/latest/youtube-dl -O ./youtube-dl
+wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O ./youtube-dl
 chmod a+rx ./youtube-dl
 ./youtube-dl -x --yes-playlist --audio-format mp3  -o 'mp3s/%(title)s.%(ext)s' $music_list
